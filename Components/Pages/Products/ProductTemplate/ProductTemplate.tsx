c176bc524361335c3022eb3daeaee2d41767619f
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-01-05 20:17:56 +0000

    routing logic

--- Components/Pages/Products/ProductTemplate/ProductTemplate.tsx
+++ Components/Pages/Products/ProductTemplate/ProductTemplate.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import * as S from './ProductTemplate.styled'
 
 const ProductTemplate = () => {
@@ -7,8 +8,12 @@ const ProductTemplate = () => {
         price: 1234,
         imageURL: 'https://image1.superdry.com/static/images/optimised/upload9223368955666189433.jpg'
     }
+    const router = useRouter()
+    const redirectToProduct = ():void => {
+        router.push(`/item/${data.id}`)
+    }
     return (
-        <S.ProductTemplateContainer className='productTemplate'>
+        <S.ProductTemplateContainer className='productTemplate' onClick={redirectToProduct}>
             <div className="image"
                 style={{
                     backgroundImage: `url('${data.imageURL}')`
