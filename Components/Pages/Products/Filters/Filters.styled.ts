0df78a2a2301c37ff1d5fed6559c76a21520775b
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-01-06 18:19:37 +0000

    chkp

--- Components/Pages/Products/Filters/Filters.styled.ts
+++ Components/Pages/Products/Filters/Filters.styled.ts
@@ -2,6 +2,51 @@ import styled from "styled-components";
 
 export const Filters = styled.div`
     width: 100%;
-    display: flex; align-items: center;
-    margin-bottom: 20px;
+    & .filters.inner{
+        width: 100%;
+        display: flex; flex-direction: row; align-items: center;
+        margin: 0 0 20px 0;
+        justify-content: space-between;
+        & .seperator{
+            display: flex; flex-direction: row; align-items: center;
+            flex-wrap: wrap;
+            & .drop-down{
+                position: relative;
+                width: 150px;
+                &:first-child{
+                    margin-right: 10px;
+                }
+                & button{
+                    padding: 15px 0;
+                    width: 100%;
+                    background-color: #eeeeee;
+                    /* border-radius: 5px; */
+                    cursor: pointer;
+                    font-size: 0.9em;
+                    font-weight: 500;
+                    border:2px solid black;
+                }
+                & .drop-down-content{
+                    display: none;
+                    position: absolute;
+                    z-index: 999;
+                    top: 100%;
+                    left: 0;
+                    height: 200px; width: 150px;
+                    overflow: scroll;
+                    background-color: white;
+                    border:2px solid black;
+                    /* border-radius: 0 0px 5px 5px; */
+                }
+                &:hover{
+                    & .drop-down-content{
+                        display: flex;
+                    }
+                    & button{
+                        /* border-radius: 5px 5px 0 0; */
+                    }
+                }
+            }
+        }
+    }
 `
--- Components/Pages/Products/Filters/Filters.tsx
+++ Components/Pages/Products/Filters/Filters.tsx
@@ -2,8 +2,25 @@ import * as S from './Filters.styled'
 
 const Filters = () => {
     return (
-        <S.Filters id='Filters'>
-
+        <S.Filters id='Filters' className='center'>
+            <div className="inner filters">
+                <div className="seperator one">
+                    <div className="drop-down sizeFilter">
+                        <button id='sizeButton'>Drop Down</button>
+                        <div className="drop-down-content"></div>
+                    </div>
+                    <div className="drop-down letterFilter">
+                        <button id='letterButton'>Drop Down</button>
+                        <div className="drop-down-content"></div>
+                    </div>
+                </div>
+                <div className="seperator two">
+                    <div className="drop-down price">
+                        <button id='priceFilter'>Drop Down</button>
+                        <div className="drop-down-content"></div>
+                    </div>
+                </div>
+            </div>
         </S.Filters>
     );
 }
