791acb15184a90cd498f5772e396f4f824b0c333
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-01-09 17:34:57 +0000

    archaic filter chain

--- clientFunctions/dBFunctions.ts
+++ clientFunctions/dBFunctions.ts
@@ -7,27 +7,37 @@ class dbFunctions{
     }
     itemSearch( search?:string,priceFilter?:string, sizeFilter?:string){
         return new Promise<any>(async (resolve, reject) => {
-            // const q = query(
-            //     collection(db, "items"),
-            //     //@ts-ignore
-            //     //orderBy('price', priceFilter), 
-            //     //where('tags', 'array-contains', search ? search : null),
-            //     //where('size', '==', sizeFilter ? sizeFilter : null)
-            // )
-            //Building the query step by step as a string and then executing it
-            let priceFilterString = ''; 
-            let sizeFilterString = '';
-            (priceFilter)
-                ? priceFilterString = ",orderBy('price', priceFilter)"
-                : null;
-            (sizeFilter)
-                ? sizeFilterString = ",where('size', '==', sizeFilter)"
-                : null;
-            const qString = `query(collection(db, "items")`+`${priceFilterString}`+`${sizeFilterString})`
-            console.log(qString)
-            // const q = eval(qString)
+            let q;
+            if(priceFilter){
+                q = query(
+                    collection(db, "items"),
+                    //@ts-ignore
+                    orderBy('price', priceFilter)
+                )
+            }
+            if(sizeFilter){
+                q = query(
+                    collection(db, "items"),
+                    //@ts-ignore
+                    where('size', '==', sizeFilter)
+                )
+            }
+            if(priceFilter && sizeFilter){
+                q = query(
+                    collection(db, "items"),
+                    //@ts-ignore,
+                    orderBy('price', priceFilter),
+                    where('size', '==', sizeFilter)
+                )
+            }
+            if(!priceFilter && !sizeFilter){
+                q = query(
+                    collection(db, "items"),
+                )
+            }
             try{
-                const rawPostData = await getDocs(eval(qString))
+                //@ts-ignore,
+                const rawPostData = await getDocs(q)
                 const data = rawPostData.docs.map(doc => ({
                     itemID: doc.id,
                     imageURL: doc.data().imageURL,
