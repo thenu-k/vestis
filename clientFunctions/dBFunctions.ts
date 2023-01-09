5e54aa8aaf04c44bca3bb53366a8c2ea844087ab
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-01-09 17:26:56 +0000

    failed chain logic

--- clientFunctions/dBFunctions.ts
+++ clientFunctions/dBFunctions.ts
@@ -5,18 +5,29 @@ import { db } from "../firebase/firebase";
 class dbFunctions{
     constructor(){
     }
-    itemSearch( search:string,priceFilter:string, sizeFilter:string){
+    itemSearch( search?:string,priceFilter?:string, sizeFilter?:string){
         return new Promise<any>(async (resolve, reject) => {
-            let q;
-            if(priceFilter){
-                //@ts-ignore
-                q = query(collection(db, "items"), orderBy('price', priceFilter))
-            }
-            else{
-                q = query(collection(db, "items"))
-            }
+            // const q = query(
+            //     collection(db, "items"),
+            //     //@ts-ignore
+            //     //orderBy('price', priceFilter), 
+            //     //where('tags', 'array-contains', search ? search : null),
+            //     //where('size', '==', sizeFilter ? sizeFilter : null)
+            // )
+            //Building the query step by step as a string and then executing it
+            let priceFilterString = ''; 
+            let sizeFilterString = '';
+            (priceFilter)
+                ? priceFilterString = ",orderBy('price', priceFilter)"
+                : null;
+            (sizeFilter)
+                ? sizeFilterString = ",where('size', '==', sizeFilter)"
+                : null;
+            const qString = `query(collection(db, "items")`+`${priceFilterString}`+`${sizeFilterString})`
+            console.log(qString)
+            // const q = eval(qString)
             try{
-                const rawPostData = await getDocs(q)
+                const rawPostData = await getDocs(eval(qString))
                 const data = rawPostData.docs.map(doc => ({
                     itemID: doc.id,
                     imageURL: doc.data().imageURL,
